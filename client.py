c0d8ff82b6a0d01fabae5544d66fe5e3a9aa8123
Viktor Aksyutin <v.aksyutin@spacevm.org>
2024-05-30 00:27:06 +0300

    fixed: logging

--- client.py
+++ client.py
@@ -95,12 +95,12 @@ class BlumClient(Session):
         if 'farming' not in self.balance_data:
             logging.info("Запуск фарминга")
             result = self.post(URL_FARMING_START)
-            logging.info(result.status_code,  result.text)
+            logging.info(f"{result.status_code},  {result.text}")
             self.update_balance()
         elif self.balance_data["timestamp"] >= self.balance_data["farming"]["endTime"]:
             logging.info('Сгребаем нафармленное')
             result = self.post(URL_FARMING_CLAIM)
-            logging.info(result.status_code,  result.text)
+            logging.info(f"{result.status_code},  {result.text}")
         logging.info(f'Ожидание завершения фарминга {self.estimate_time} секунд')
         sleep(self.estimate_time)
 
